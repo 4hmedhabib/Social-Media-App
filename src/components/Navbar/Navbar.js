d2fc9db262d6cedad16203406e25a9214e222748
ahmedhabiib <info.ahmed.developer@gmail.com>
2021-07-22 15:02:05 +0300

    added reactbootstrap and navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,26 @@
-const Navbar = () => {
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <div className="container">
-          <a href="" className="text-decoration-none">
+      <Navbar collapseOnSelect expand="sm" bg="light" variant="light">
+        <Container justify-content-center align-items-center>
+          <Navbar.Brand className="h3" href="/">
             Social Media
-          </a>
-        </div>
-      </nav>
+          </Navbar.Brand>
+          <Navbar.Toggle area-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/create">Create Post</Nav.Link>
+              <Nav.Link href="/profile">Current User</Nav.Link>
+              <Nav.Link href="/users">All Users</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
 
-export default Navbar;
+export default Header;
