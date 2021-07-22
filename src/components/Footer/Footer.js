c1393cac0c757322fdff79a43c655c5e5f503965
ahmedhabiib <info.ahmed.developer@gmail.com>
2021-07-22 14:17:53 +0300

    setup layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark p-3">
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <div className="container">
+          <a href="" className="text-decoration-none">
+            Social Media
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
